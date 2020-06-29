4037caee0ce46651b3a14e5aa772b211657b40e4
Sherlock <Sherlock-Hou@github.com>
2020-06-29 22:56:20 +0800

    Update cma.py

--- deap/cma.py
+++ deap/cma.py
@@ -16,7 +16,6 @@
 #    Special thanks to Nikolaus Hansen for providing major part of
 #    this code. The CMA-ES algorithm is provided in many other languages
 #    and advanced versions at http://www.lri.fr/~hansen/cmaesintro.html.
-
 """A module that provides support for the Covariance Matrix Adaptation
 Evolution Strategy.
 """
@@ -91,14 +90,14 @@ class Strategy(object):
         self.sigma = sigma
         self.pc = numpy.zeros(self.dim)
         self.ps = numpy.zeros(self.dim)
-        self.chiN = sqrt(self.dim) * (1 - 1. / (4. * self.dim) +
-                                      1. / (21. * self.dim ** 2))
+        self.chiN = sqrt(self.dim) * (1 - 1. / (4. * self.dim) + 1. /
+                                      (21. * self.dim**2))
 
         self.C = self.params.get("cmatrix", numpy.identity(self.dim))
         self.diagD, self.B = numpy.linalg.eigh(self.C)
 
         indx = numpy.argsort(self.diagD)
-        self.diagD = self.diagD[indx] ** 0.5
+        self.diagD = self.diagD[indx]**0.5
         self.B = self.B[:, indx]
         self.BD = self.B * self.diagD
 
@@ -140,9 +139,12 @@ class Strategy(object):
             * numpy.dot(self.B, (1. / self.diagD) *
                         numpy.dot(self.B.T, c_diff))
 
-        hsig = float((numpy.linalg.norm(self.ps) /
-                      sqrt(1. - (1. - self.cs) ** (2. * (self.update_count + 1.))) / self.chiN <
-                      (1.4 + 2. / (self.dim + 1.))))
+        hsig = float(
+            (numpy.linalg.norm(self.ps) /
+             sqrt(1. -
+                  (1. - self.cs)**(2. *
+                                   (self.update_count + 1.))) / self.chiN <
+             (1.4 + 2. / (self.dim + 1.))))
 
         self.update_count += 1
 
@@ -166,7 +168,7 @@ class Strategy(object):
 
         self.cond = self.diagD[indx[-1]] / self.diagD[indx[0]]
 
-        self.diagD = self.diagD[indx] ** 0.5
+        self.diagD = self.diagD[indx]**0.5
         self.B = self.B[:, indx]
         self.BD = self.B * self.diagD
 
@@ -189,19 +191,20 @@ class Strategy(object):
             raise RuntimeError("Unknown weights : %s" % rweights)
 
         self.weights /= sum(self.weights)
-        self.mueff = 1. / sum(self.weights ** 2)
+        self.mueff = 1. / sum(self.weights**2)
 
         self.cc = params.get("ccum", 4. / (self.dim + 4.))
-        self.cs = params.get("cs", (self.mueff + 2.) /
-                             (self.dim + self.mueff + 3.))
-        self.ccov1 = params.get("ccov1", 2. / ((self.dim + 1.3) ** 2 +
-                                               self.mueff))
-        self.ccovmu = params.get("ccovmu", 2. * (self.mueff - 2. +
-                                                 1. / self.mueff) /
-                                 ((self.dim + 2.) ** 2 + self.mueff))
+        self.cs = params.get("cs",
+                             (self.mueff + 2.) / (self.dim + self.mueff + 3.))
+        self.ccov1 = params.get("ccov1",
+                                2. / ((self.dim + 1.3)**2 + self.mueff))
+        self.ccovmu = params.get(
+            "ccovmu", 2. * (self.mueff - 2. + 1. / self.mueff) /
+            ((self.dim + 2.)**2 + self.mueff))
         self.ccovmu = min(1 - self.ccov1, self.ccovmu)
-        self.damps = 1. + 2. * max(0, sqrt((self.mueff - 1.) /
-                                           (self.dim + 1.)) - 1.) + self.cs
+        self.damps = 1. + 2. * max(
+            0,
+            sqrt((self.mueff - 1.) / (self.dim + 1.)) - 1.) + self.cs
         self.damps = params.get("damps", self.damps)
 
 
@@ -268,11 +271,12 @@ class StrategyOnePlusLambda(object):
         # Step size control :
         self.d = params.get("d", 1.0 + self.dim / (2.0 * self.lambda_))
         self.ptarg = params.get("ptarg", 1.0 / (5 + sqrt(self.lambda_) / 2.0))
-        self.cp = params.get("cp", self.ptarg * self.lambda_ / (2 + self.ptarg * self.lambda_))
+        self.cp = params.get(
+            "cp", self.ptarg * self.lambda_ / (2 + self.ptarg * self.lambda_))
 
         # Covariance matrix adaptation
         self.cc = params.get("cc", 2.0 / (self.dim + 2.0))
-        self.ccov = params.get("ccov", 2.0 / (self.dim ** 2 + 6.0))
+        self.ccov = params.get("ccov", 2.0 / (self.dim**2 + 6.0))
         self.pthresh = params.get("pthresh", 0.44)
 
     def generate(self, ind_init):
@@ -296,7 +300,8 @@ class StrategyOnePlusLambda(object):
                            parameters.
         """
         population.sort(key=lambda ind: ind.fitness, reverse=True)
-        lambda_succ = sum(self.parent.fitness <= ind.fitness for ind in population)
+        lambda_succ = sum(self.parent.fitness <= ind.fitness
+                          for ind in population)
         p_succ = float(lambda_succ) / self.lambda_
         self.psucc = (1 - self.cp) * self.psucc + self.cp * p_succ
 
@@ -304,13 +309,19 @@ class StrategyOnePlusLambda(object):
             x_step = (population[0] - numpy.array(self.parent)) / self.sigma
             self.parent = copy.deepcopy(population[0])
             if self.psucc < self.pthresh:
-                self.pc = (1 - self.cc) * self.pc + sqrt(self.cc * (2 - self.cc)) * x_step
-                self.C = (1 - self.ccov) * self.C + self.ccov * numpy.outer(self.pc, self.pc)
+                self.pc = (1 - self.cc) * self.pc + sqrt(
+                    self.cc * (2 - self.cc)) * x_step
+                self.C = (1 - self.ccov) * self.C + self.ccov * numpy.outer(
+                    self.pc, self.pc)
             else:
                 self.pc = (1 - self.cc) * self.pc
-                self.C = (1 - self.ccov) * self.C + self.ccov * (numpy.outer(self.pc, self.pc) + self.cc * (2 - self.cc) * self.C)
+                self.C = (1 - self.ccov) * self.C + self.ccov * (
+                    numpy.outer(self.pc, self.pc) + self.cc *
+                    (2 - self.cc) * self.C)
 
-        self.sigma = self.sigma * exp(1.0 / self.d * (self.psucc - self.ptarg) / (1.0 - self.ptarg))
+        self.sigma = self.sigma * exp(1.0 / self.d *
+                                      (self.psucc - self.ptarg) /
+                                      (1.0 - self.ptarg))
 
         # We use Cholesky since for now we have no use of eigen decomposition
         # Basically, Cholesky returns a matrix A as C = A*A.T
@@ -377,7 +388,7 @@ class StrategyMultiObjective(object):
 
         # Covariance matrix adaptation
         self.cc = params.get("cc", 2.0 / (self.dim + 2.0))
-        self.ccov = params.get("ccov", 2.0 / (self.dim ** 2 + 6.0))
+        self.ccov = params.get("ccov", 2.0 / (self.dim**2 + 6.0))
         self.pthresh = params.get("pthresh", 0.44)
 
         # Internal parameters associated to the mu parent
@@ -385,7 +396,9 @@ class StrategyMultiObjective(object):
         # Lower Cholesky matrix (Sampling matrix)
         self.A = [numpy.identity(self.dim) for _ in range(len(population))]
         # Inverse Cholesky matrix (Used in the update of A)
-        self.invCholesky = [numpy.identity(self.dim) for _ in range(len(population))]
+        self.invCholesky = [
+            numpy.identity(self.dim) for _ in range(len(population))
+        ]
         self.pc = [numpy.zeros(self.dim) for _ in range(len(population))]
         self.psucc = [self.ptarg] * len(population)
 
@@ -413,16 +426,22 @@ class StrategyMultiObjective(object):
         if self.lambda_ == self.mu:
             for i in range(self.lambda_):
                 # print "Z", list(arz[i])
-                individuals.append(ind_init(self.parents[i] + self.sigmas[i] * numpy.dot(self.A[i], arz[i])))
+                individuals.append(
+                    ind_init(self.parents[i] +
+                             self.sigmas[i] * numpy.dot(self.A[i], arz[i])))
                 individuals[-1]._ps = "o", i
 
         # Parents producing an offspring are chosen at random from the first front
         else:
-            ndom = tools.sortLogNondominated(self.parents, len(self.parents), first_front_only=True)
+            ndom = tools.sortLogNondominated(self.parents,
+                                             len(self.parents),
+                                             first_front_only=True)
             for i in range(self.lambda_):
                 j = numpy.random.randint(0, len(ndom))
                 _, p_idx = ndom[j]._ps
-                individuals.append(ind_init(self.parents[p_idx] + self.sigmas[p_idx] * numpy.dot(self.A[p_idx], arz[i])))
+                individuals.append(
+                    ind_init(self.parents[p_idx] + self.sigmas[p_idx] *
+                             numpy.dot(self.A[p_idx], arz[i])))
                 individuals[-1]._ps = "o", p_idx
 
         return individuals
@@ -474,13 +493,14 @@ class StrategyMultiObjective(object):
         # Under this threshold, the update is mostly noise
         if w.max() > 1e-20:
             w_inv = numpy.dot(w, invCholesky)
-            norm_w2 = numpy.sum(w ** 2)
+            norm_w2 = numpy.sum(w**2)
             a = sqrt(alpha)
             root = numpy.sqrt(1 + beta / alpha * norm_w2)
             b = a / norm_w2 * (root - 1)
 
             A = a * A + b * numpy.outer(v, w)
-            invCholesky = 1.0 / a * invCholesky - b / (a ** 2 + a * b * norm_w2) * numpy.outer(w, w_inv)
+            invCholesky = 1.0 / a * invCholesky - b / (
+                a**2 + a * b * norm_w2) * numpy.outer(w, w_inv)
 
         return invCholesky, A
 
@@ -497,12 +517,30 @@ class StrategyMultiObjective(object):
         d, ptarg, pthresh = self.d, self.ptarg, self.pthresh
 
         # Make copies for chosen offspring only
-        last_steps = [self.sigmas[ind._ps[1]] if ind._ps[0] == "o" else None for ind in chosen]
-        sigmas = [self.sigmas[ind._ps[1]] if ind._ps[0] == "o" else None for ind in chosen]
-        invCholesky = [self.invCholesky[ind._ps[1]].copy() if ind._ps[0] == "o" else None for ind in chosen]
-        A = [self.A[ind._ps[1]].copy() if ind._ps[0] == "o" else None for ind in chosen]
-        pc = [self.pc[ind._ps[1]].copy() if ind._ps[0] == "o" else None for ind in chosen]
-        psucc = [self.psucc[ind._ps[1]] if ind._ps[0] == "o" else None for ind in chosen]
+        last_steps = [
+            self.sigmas[ind._ps[1]] if ind._ps[0] == "o" else None
+            for ind in chosen
+        ]
+        sigmas = [
+            self.sigmas[ind._ps[1]] if ind._ps[0] == "o" else None
+            for ind in chosen
+        ]
+        invCholesky = [
+            self.invCholesky[ind._ps[1]].copy() if ind._ps[0] == "o" else None
+            for ind in chosen
+        ]
+        A = [
+            self.A[ind._ps[1]].copy() if ind._ps[0] == "o" else None
+            for ind in chosen
+        ]
+        pc = [
+            self.pc[ind._ps[1]].copy() if ind._ps[0] == "o" else None
+            for ind in chosen
+        ]
+        psucc = [
+            self.psucc[ind._ps[1]] if ind._ps[0] == "o" else None
+            for ind in chosen
+        ]
 
         # Update the internal parameters for successful offspring
         for i, ind in enumerate(chosen):
@@ -512,20 +550,26 @@ class StrategyMultiObjective(object):
             if t == "o":
                 # Update (Success = 1 since it is chosen)
                 psucc[i] = (1.0 - cp) * psucc[i] + cp
-                sigmas[i] = sigmas[i] * exp((psucc[i] - ptarg) / (d * (1.0 - ptarg)))
+                sigmas[i] = sigmas[i] * exp(
+                    (psucc[i] - ptarg) / (d * (1.0 - ptarg)))
 
                 if psucc[i] < pthresh:
                     xp = numpy.array(ind)
                     x = numpy.array(self.parents[p_idx])
-                    pc[i] = (1.0 - cc) * pc[i] + sqrt(cc * (2.0 - cc)) * (xp - x) / last_steps[i]
-                    invCholesky[i], A[i] = self._rankOneUpdate(invCholesky[i], A[i], 1 - ccov, ccov, pc[i])
+                    pc[i] = (1.0 - cc) * pc[i] + sqrt(
+                        cc * (2.0 - cc)) * (xp - x) / last_steps[i]
+                    invCholesky[i], A[i] = self._rankOneUpdate(
+                        invCholesky[i], A[i], 1 - ccov, ccov, pc[i])
                 else:
                     pc[i] = (1.0 - cc) * pc[i]
                     pc_weight = cc * (2.0 - cc)
-                    invCholesky[i], A[i] = self._rankOneUpdate(invCholesky[i], A[i], 1 - ccov + pc_weight, ccov, pc[i])
+                    invCholesky[i], A[i] = self._rankOneUpdate(
+                        invCholesky[i], A[i], 1 - ccov + pc_weight, ccov,
+                        pc[i])
 
                 self.psucc[p_idx] = (1.0 - cp) * self.psucc[p_idx] + cp
-                self.sigmas[p_idx] = self.sigmas[p_idx] * exp((self.psucc[p_idx] - ptarg) / (d * (1.0 - ptarg)))
+                self.sigmas[p_idx] = self.sigmas[p_idx] * exp(
+                    (self.psucc[p_idx] - ptarg) / (d * (1.0 - ptarg)))
 
         # It is unnecessary to update the entire parameter set for not chosen individuals
         # Their parameters will not make it to the next generation
@@ -535,13 +579,30 @@ class StrategyMultiObjective(object):
             # Only the offspring update the parameter set
             if t == "o":
                 self.psucc[p_idx] = (1.0 - cp) * self.psucc[p_idx]
-                self.sigmas[p_idx] = self.sigmas[p_idx] * exp((self.psucc[p_idx] - ptarg) / (d * (1.0 - ptarg)))
+                self.sigmas[p_idx] = self.sigmas[p_idx] * exp(
+                    (self.psucc[p_idx] - ptarg) / (d * (1.0 - ptarg)))
 
         # Make a copy of the internal parameters
         # The parameter is in the temporary variable for offspring and in the original one for parents
         self.parents = chosen
-        self.sigmas = [sigmas[i] if ind._ps[0] == "o" else self.sigmas[ind._ps[1]] for i, ind in enumerate(chosen)]
-        self.invCholesky = [invCholesky[i] if ind._ps[0] == "o" else self.invCholesky[ind._ps[1]] for i, ind in enumerate(chosen)]
-        self.A = [A[i] if ind._ps[0] == "o" else self.A[ind._ps[1]] for i, ind in enumerate(chosen)]
-        self.pc = [pc[i] if ind._ps[0] == "o" else self.pc[ind._ps[1]] for i, ind in enumerate(chosen)]
-        self.psucc = [psucc[i] if ind._ps[0] == "o" else self.psucc[ind._ps[1]] for i, ind in enumerate(chosen)]
+        self.sigmas = [
+            sigmas[i] if ind._ps[0] == "o" else self.sigmas[ind._ps[1]]
+            for i, ind in enumerate(chosen)
+        ]
+        self.invCholesky = [
+            invCholesky[i]
+            if ind._ps[0] == "o" else self.invCholesky[ind._ps[1]]
+            for i, ind in enumerate(chosen)
+        ]
+        self.A = [
+            A[i] if ind._ps[0] == "o" else self.A[ind._ps[1]]
+            for i, ind in enumerate(chosen)
+        ]
+        self.pc = [
+            pc[i] if ind._ps[0] == "o" else self.pc[ind._ps[1]]
+            for i, ind in enumerate(chosen)
+        ]
+        self.psucc = [
+            psucc[i] if ind._ps[0] == "o" else self.psucc[ind._ps[1]]
+            for i, ind in enumerate(chosen)
+        ]
